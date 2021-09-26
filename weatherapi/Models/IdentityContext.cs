003aede500900114dd59f607525379824c90b933
Samira Maleki <samira.maleki1112@gmail.com>
2021-09-27 00:36:03 +0330

    Add project files.

---
+++ weatherapi/Models/IdentityContext.cs
@@ -0,0 +1,29 @@
+﻿using DataAccess.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace weatherapi.Models
+{
+    public class IdentityContext : DbContext
+    {
+        public IdentityContext()
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+        public IdentityContext(DbContextOptions<IdentityContext> dbContextOptions)
+          : base(dbContextOptions)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
